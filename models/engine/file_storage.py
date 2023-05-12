895e417d8aaec289ae18d6d32e34692d2b028881
Ehigboria Dukeson O <studysampul@gmail.com>
2023-05-12 12:14:07 +0100

    Solution to Task 5 - File Storage in the AirBnB Clone Project

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Importing some Standard modules and modules from our packages"""
 import json
-from models.base_model import BaseModel as bm
+from models.base_model import BaseModel
 
 """
 This is a Python class that will be responsible for file storage. In this
@@ -17,14 +17,44 @@ class FileStorage():
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        ...
+    def all(self) -> dict:
+        """
+        This is a public instance method that returns the private instance
+         attribute `__object` which is a dictionary"""
+        return self.__objects
 
-    def new(self):
-        ...
+    def new(self, obj: dict) -> None:
+        """
+        This is a public instance method that sets in `__objects` `obj` with
+        the key
+        Args:
+            obj (dict) - a dictionaary object
+        """
+        fmt = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[fmt] = obj
 
-    def save(self):
-        ...
+    def save(self) -> None:
+        """
+        This is a public instance method that serializes the private instance
+        object `__objects` (dict) into a JSON string and save it to a flat
+        database (json file)
+        """
+        dict_serial = {}
+        with open(self.__file_path, mode="w", encoding="utf-8") as fn:
+            for key, val in self.__objects.items():
+                dict_serial[key] = val.to_dict()
+            json.dump(dict_serial, fn, indent=1)
 
-    def reload(self):
-        ...
+    def reload(self) -> None:
+        """
+        This is a public instance method that deserializes a json string into
+        a dictionary object `__objects` only if `__file_path` exist.
+        """
+        try:
+            with open(self.__file_path, mode="r", encoding="utf-8") as fn:
+                data_strm = json.load(fn)
+            for key, val in data_strm.items():
+                class_name = key.split(".")[0]
+                self.new(eval(class_name + "(**val)"))
+        except FileNotFoundError:
+            ...
